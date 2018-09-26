d2c1dd1eef49762800d9f4ef609b127e9a59848c
Brandon Jernigan <bjernigan@netspend.com>
2018-09-26 17:33:54 -0500

    Converted to class to implement length functionality, added/updated unit tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,13 @@
-import { storageFactory } from "./index";
+import { StorageFactory } from "./index";
 
 describe("storageFactory", () => {
-  describe("when storage is not supported", () => {
-    const storage = storageFactory({} as Storage);
+  let storage = new StorageFactory({} as Storage);
 
+  beforeEach(() => {
+    storage = new StorageFactory({} as Storage);
+  });
+
+  describe("when storage is not supported", () => {
     describe("setItem and getItem", () => {
       it("sets values and returns", () => {
         expect(storage.setItem("foo", "Foo")).toBeUndefined();
@@ -27,9 +31,38 @@ describe("storageFactory", () => {
         expect(storage.getItem("undefined")).toEqual("undefined");
       });
     });
+    describe("property: length", () => {
+      it("is initialized at 0", () => {
+        expect(storage.length).toEqual(0);
+      });
+      it("should increment with setItem", () => {
+        storage.setItem("foo", "Foo");
+        expect(storage.length).toEqual(1);
+      });
+      it("should reset to 0 when cleared", () => {
+        storage.setItem("foo", "Foo");
+        expect(storage.length).toEqual(1);
+        storage.clear();
+        expect(storage.length).toEqual(0);
+      });
+    });
   });
 
-  describe.skip("when storage is supported", () => {
-    // then we are just fine 😅
+  describe("when storage is supported", () => {
+    describe("property: length", () => {
+      it("is initialized at 0", () => {
+        expect(storage.length).toEqual(0);
+      });
+      it("should increment with setItem", () => {
+        storage.setItem("foo", "Foo");
+        expect(storage.length).toEqual(1);
+      });
+      it("should reset to 0 when cleared", () => {
+        storage.setItem("foo", "Foo");
+        expect(storage.length).toEqual(1);
+        storage.clear();
+        expect(storage.length).toEqual(0);
+      });
+    });
   });
 });
